222df2b86b5cbae2435be62512e2bc7c6c99c086
Jerry Yang <efjerryyang@outlook.com>
2023-04-04 22:11:40 +0800

    feat: add two example alphas and corresponding expressions

--- src/simulator/simulate.py
+++ src/simulator/simulate.py
@@ -333,7 +333,27 @@ class Simulator:
 def example_alpha(prev_day: str, df: pd.DataFrame) -> pd.DataFrame:
     close = df.pivot(index="date", columns="symbol", values="close")
     volume = df.pivot(index="date", columns="symbol", values="volume")
+
     df = -rank(ts_delta(close, 2)) * rank(volume / ts_sum(volume, 30) / 30)
+    
+    df = df.loc[pd.Timestamp(prev_day).date()]
+    return df
+
+
+def example_alpha2(prev_day: str, df: pd.DataFrame) -> pd.DataFrame:
+    close = df.pivot(index="date", columns="symbol", values="close")
+    
+    df = -(close - ts_mean(close, 5))
+    
+    df = df.loc[pd.Timestamp(prev_day).date()]
+    return df
+
+
+def example_alpha3(prev_day: str, df: pd.DataFrame) -> pd.DataFrame:
+    close = df.pivot(index="date", columns="symbol", values="close")
+    
+    df = rank(-(close - ts_mean(close, 10)))
+    
     df = df.loc[pd.Timestamp(prev_day).date()]
     return df
 
