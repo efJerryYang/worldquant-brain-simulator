ad1b01b9e64f605b78d7c835ece36074d1a86976
Jerry Yang <efjerryyang@outlook.com>
2023-04-06 16:42:05 +0800

    feat: add points data from platform simulations

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from alpha_pool.alpha import *
 
 s = Simulator()
 
-s.simulate_with_multiprocessing(eg_alpha)
+# s.simulate_with_multiprocessing(eg_alpha)
 s.simulate_with_multiprocessing(eg_alpha2)
 s.simulate_with_multiprocessing(eg_alpha3)
 # s.simulate(eg_alpha)
