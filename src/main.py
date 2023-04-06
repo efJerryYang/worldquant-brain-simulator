dd8b8129f7b28ec662933819dee2e4ec712658be
Jerry Yang <efjerryyang@outlook.com>
2023-04-06 13:13:25 +0800

    chore: run insample simulations

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from alpha_pool.alpha import *
 
 s = Simulator()
 
-s.simulate_with_multiprocessing(eg_alpha)
-s.simulate_with_multiprocessing(eg_alpha2)
-s.simulate_with_multiprocessing(eg_alpha3)
-# s.simulate(eg_alpha)
+# s.simulate_with_multiprocessing(eg_alpha)
+# s.simulate_with_multiprocessing(eg_alpha2)
+# s.simulate_with_multiprocessing(eg_alpha3)
+s.simulate(eg_alpha)
+s.simulate(eg_alpha2)
+s.simulate(eg_alpha3)
